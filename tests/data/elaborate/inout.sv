963607d29150eb5c8be114bfbc45d95c5463637e
wolf-tungsten <gaoruihao@ict.ac.cn>
2026-02-09 15:52:28 +0800

    chore: remove useless test data

--- tests/data/elaborate/inout.sv
+++
@@ -1,9 +0,0 @@
-module inout_case(
-    input  logic        en,
-    input  logic [3:0]  data,
-    inout  wire  [3:0]  io,
-    output logic [3:0]  io_in
-);
-    assign io = en ? data : 4'bz;
-    assign io_in = io;
-endmodule
--- tests/data/elaborate/t0.sv
+++
@@ -1,6 +0,0 @@
-module t0 #(parameter [3:0] P = 16) (
-    input  wire [7:0] i_port,
-    output wire [7:0] o_port
-);
-    assign o_port = ~i_port + P;
-endmodule
